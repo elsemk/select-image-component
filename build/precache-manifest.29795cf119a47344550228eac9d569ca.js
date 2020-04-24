05830cf275d0d16cdeaa152d39dc9f7d1bc15b90
“kentMa” <kang.ma@socialcredits.cn>
2020-04-24 21:16:01 +0800

    上传build

---
+++ build/precache-manifest.29795cf119a47344550228eac9d569ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd8d2b52181d8b39b87fa2ade6c8642",
+    "url": "/select-image-component/index.html"
+  },
+  {
+    "revision": "b937e5d2693bfcce9278",
+    "url": "/select-image-component/static/css/main.c24b94db.chunk.css"
+  },
+  {
+    "revision": "119931cb81f229ef7751",
+    "url": "/select-image-component/static/js/2.817f6c6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/select-image-component/static/js/2.817f6c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b937e5d2693bfcce9278",
+    "url": "/select-image-component/static/js/main.54374784.chunk.js"
+  },
+  {
+    "revision": "38118e23b96c5610f140",
+    "url": "/select-image-component/static/js/runtime-main.0185e37c.js"
+  }
+]);
